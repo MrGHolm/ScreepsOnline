e74115bf57fd5e821a30d26d2e62b6742e6355f4
Gustav Holm <gustavholm123@hotmail.com>
2025-12-18 13:36:03 +0100

    added aoutobuild for containers and check for harvesters

--- m.rooms.js
+++ m.rooms.js
@@ -7,12 +7,22 @@ let MRooms = {
                 let mSourceData = {
                     index: mS,
                     id: mSources[mS].id,
-                    pos: mSources[mS].pos.findPathTo(myRoom.spawn.pos)[0]
+                    pos: mSources[mS].pos.findPathTo(myRoom.spawn.pos,)[0]
                 };
                 mSourceArray.push(mSourceData);
             }
             myRoom.memory.mySources = mSourceArray;
+            for (let memS in myRoom.memory.mySources) {
+                let lMemS = myRoom.memory.mySources[memS];
+                if (myRoom.pos.lookForAt(LOOK_STRUCTURES, lMemS.pos).structureType !== STRUCTURE_CONTAINER ||
+                    myRoom.pos.lookForAt(LOOK_CONSTRUCTION_SITES, lMemS.pos).structureType !== STRUCTURE_CONTAINER) {
+                    myRoom.pos.createConstructionSite(lMemS.pos, STRUCTURE_CONTAINER)
+                }
+            }
             let mHarvesters = myRoom.find(FIND_MY_CREEPS, {filter: (mc) => mc.memory.role === 'harvester'})
+            if (mHarvesters.length < mSourceArray.length) {
+                
+            }
         }
     }
 }
