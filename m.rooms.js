66758fc5a13db23458c5b07d009dd91657002d23
Gustav Holm <gustavholm123@hotmail.com>
2025-12-19 11:33:58 +0100

    more try fixing pos error

--- m.rooms.js
+++ m.rooms.js
@@ -5,21 +5,23 @@ let MRooms = {
             let mSourceArray = []
             for (let mS in mSources) {
                 console.log('pos1 : ' + mSources[mS]);
-                console.log(' : ' + myRoom[mS].find(FIND_MY_SPAWNS));
+                console.log('myRoom position : ' + myRoom[mS].find(FIND_MY_SPAWNS));
                 let mSourceData = {
                     index: mS,
                     id: mSources[mS].id,
-                    pos: mSources[mS].pos.findPathTo(myRoom[mS].find(FIND_MY_SPAWNS)[0])
+                    xPos: mSources[mS].findPathTo(myRoom.find(FIND_MY_SPAWNS)[0]).x,
+                    yPos: mSources[mS].findPathTo(myRoom.find(FIND_MY_SPAWNS)[0]).y
                 };
                 mSourceArray.push(mSourceData);
             }
             myRoom.memory.mySources = mSourceArray;
+            console.log('mSourceArray : '+mSourceArray);
             for (let memS in myRoom.memory.mySources) {
                 let lMemS = myRoom.memory.mySources[memS];
                 console.log('pos2')
-                if (myRoom.pos.lookForAt(LOOK_STRUCTURES, lMemS.pos).structureType !== STRUCTURE_CONTAINER ||
-                    myRoom.pos.lookForAt(LOOK_CONSTRUCTION_SITES, lMemS.pos).structureType !== STRUCTURE_CONTAINER) {
-                    myRoom.pos.createConstructionSite(lMemS.pos, STRUCTURE_CONTAINER)
+                if (myRoom.pos.lookForAt(LOOK_STRUCTURES, lMemS.xPos, lMemS.yPos).structureType !== STRUCTURE_CONTAINER ||
+                    myRoom.pos.lookForAt(LOOK_CONSTRUCTION_SITES, lMemS.xPos, lMemS.yPos).structureType !== STRUCTURE_CONTAINER) {
+                    myRoom.pos.createConstructionSite(lMemS.xPos, lMemS.yPos, STRUCTURE_CONTAINER);
                 }
             }
             console.log('pos3')
