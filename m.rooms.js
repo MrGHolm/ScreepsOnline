48822e77c38bf11636132010db1782b4fe6ea2e5
Gustav Holm <gustavholm123@hotmail.com>
2025-12-20 02:03:11 +0100

    rooms fixed

--- m.rooms.js
+++ m.rooms.js
@@ -5,33 +5,37 @@ let MRooms = {
             let mSourceArray = []
             let mSpawnArray = myRoom.find(FIND_MY_SPAWNS)
             for (let mS in mSources) {
-                console.log('pos1 : ' + mSources[mS]);
-                console.log('myRoom position : ' + myRoom[mS].find(FIND_MY_SPAWNS));
+                //console.log('pos1 : ' + mSources[mS]);
+                //console.log('myRoom position : ' + myRoom);
                 let mSourceData = {
                     index: mS,
                     id: mSources[mS].id,
-                    pos: mSources[mS].findPathTo(mSources[mS].pos.findClosestByRange(mSpawnArray).pos)[0]
+                    pos: mSources[mS].pos.findPathTo(mSources[mS].pos.findClosestByRange(mSpawnArray).pos)[0]
 
                 };
-                console.log('mSourceData pos '+ mS + 1 +': '+mSourceData.pos)
+                //console.log('mSourceData pos '+ mS + 1 +': '+mSourceData.pos)
                 mSourceArray.push(mSourceData);
             }
             myRoom.memory.mySources = mSourceArray;
-            console.log('mSourceArray : '+mSourceArray);
+            //console.log('mSourceArray : '+mSourceArray);
             for (let memS in myRoom.memory.mySources) {
                 let lMemS = myRoom.memory.mySources[memS];
-                console.log('pos2')
-                if (myRoom.pos.lookForAt(LOOK_STRUCTURES, lMemS.pos).structureType !== STRUCTURE_CONTAINER ||
-                    myRoom.pos.lookForAt(LOOK_CONSTRUCTION_SITES, lMemS.pos).structureType !== STRUCTURE_CONTAINER) {
-                    myRoom.pos.createConstructionSite( lMemS.pos, STRUCTURE_CONTAINER);
+                //console.log('pos2')
+                if (myRoom.lookForAt(LOOK_STRUCTURES, lMemS.pos.x, lMemS.pos.y).structureType !== STRUCTURE_CONTAINER ||
+                    myRoom.lookForAt(LOOK_CONSTRUCTION_SITES, lMemS.pos.x, lMemS.pos.y).structureType !== STRUCTURE_CONTAINER) {
+                    myRoom.createConstructionSite( lMemS.pos.x, lMemS.pos.y, STRUCTURE_CONTAINER);
                 }
             }
-            console.log('pos3')
+            //console.log('pos3')
             let mHarvesters = myRoom.find(FIND_MY_CREEPS, {filter: (mc) => mc.memory.role === 'harvester'})
             if (mHarvesters.length < mSourceArray.length) {
                 let que = Memory.spawnCreepsQue;
+                if (que === undefined) {
+                    que = []
+                }
+                //console.log(que)
                 if (!que.includes('harvester')) {
-                    que.append('harvester');
+                    que.push('harvester');
                     Memory.spawnCreepsQue = que;
                 }
             }
